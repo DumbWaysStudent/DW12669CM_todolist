5f94be6c90e909fce7ec6ef2ed3e208b91946be2
Cerkhachacu <54925437+Cerkhachacu@users.noreply.github.com>
2019-10-06 23:13:20 +0700

    make_list

--- App.js
+++ App.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import {
+  SafeAreaView, 
+  Text,
+  View,
+  StyleSheet,
+} from 'react-native'
 
-import Tugas1 from './makeToDo'
-import Tugas2 from './addToDo'
-import Tugas3 from './deleteToDo'
+export default class App extends Component {
+   constructor(props){
+     super(props);
+     this.state = {
+       newTodoList: '',
+       toDoList: [
+         {'id':1,'task':'work'},
+         {'id':2,'task':'Swim'},
+         {'id':3,'task':'Study'},
+         {'id':4,'task':'sleep'}
+        ]
+     }
+   }
 
-class App extends Component {
-    render() {
-        return (
-            <View>
-                <Tugas3 />
-            </View>
-        )
-    }
+  render() {
+    return (
+      <SafeAreaView>
+         <View >
+          {this.state.toDoList.map( (isi,i) =>
+          <View key= {i} style = {style.bottomBorder}>
+           <Text key = {isi.id}
+           style = {[style.textPadding,style.textStyle]}>
+           {isi.task}
+           </Text>
+          </View> 
+           )}
+        </View>
+      </SafeAreaView>
+
+    )
+  }
+  
+
 }
 
-export default App;
+const style = StyleSheet.create({
+  row:{
+    flexDirection: 'row',
+  },
+  textPadding:{
+    paddingLeft: 10,
+  },
+  textStyle:{
+    fontSize: 30,
+    fontWeight: '300',
+    marginBottom: 10,
+  },
+  bottomBorder:{
+    borderBottomColor: 'black',
+    borderBottomWidth: 2,
+  }
+})
